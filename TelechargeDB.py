dea6c67efe3b71cfd072f8add7f1675d51cd3ffe
Juan Bueno <juigbueno@gmail.com>
2020-09-08 11:10:14 +0200

    Include harness of table v

--- TelechargeDB.py
+++ TelechargeDB.py
@@ -10,6 +10,7 @@ class DataBase:
         engine_name = 'sqlite:///telecharge.db'
         self.engine = create_engine(engine_name, echo=True)
         self.metadata = MetaData(bind=None)
+        # TABLA E: TABLA DE DATOS DE ESTACIÓN
         Table('Table_E', self.metadata,
               Column('EESS', String(7), nullable=False),   # Número de estación H24.
               Column('CIF', String(10), nullable=False),   # CIF de la estación de servicio
@@ -25,11 +26,13 @@ class DataBase:
               Column('LANG', String(2), nullable=False),  # Idioma del TPV
               Column('CURR', String(3), nullable=False)   # Divisa del TPV
               )
+        # TABLA u: TABLA PARAMETROS DE COMUNICACIÓN
         Table('Table_u', self.metadata,
               Column('NEMO_ACCESO', String(3), nullable=False),   # Tag tipo de acceso permitido
               Column('NEMO_VALOR', String(3), nullable=False),   # Tag indicativo del parámetro
               Column('VALOR', String(50), nullable=False)  # Valor del parámetro
               )
+        # TABLA F: TABLA DE BINES
         Table('Table_F', self.metadata,
               Column('Bin', String(6), nullable=False),   # Numero de Bin
               Column('Operativa', String(1), nullable=False),   # Operativa para el despliegue del menu
@@ -49,11 +52,13 @@ class DataBase:
               Column('Longitud', String(2), nullable=False),  # Longitud del PAN para su validación.
               Column('Acción', String(1), nullable=False)  # Alta, Baja o Modificación.
               )
+        # TABLA x: TABLA BINES PAGO EMV
         Table('Table_x', self.metadata,
               Column('Lognitud BIN', String(1), nullable=False),   # Longitud del BIN
               Column('BIN', String(6), nullable=False)   # BIN sobre el cual se tiene que hacer el control del código
               # de servicio.
               )
+        # TABLA m: TABLA DE PARAMETROS GENERALES EMV
         Table('Table_m', self.metadata,
               Column('Terminal Type', String(2), nullable=False),   # Tag 9F35: según ICS
               Column('Terminal Capabilities', String(6), nullable=False),  # Tag 9F33: según ICS
@@ -63,6 +68,7 @@ class DataBase:
               Column('Terminal Country Code', String(3), nullable=False),  # Tag 9F1A
               Column('Flags EMV', String(2), nullable=False)   # Ver tabla siguiente ¿?
               )
+        # TABLA v: TABLA DE APLICACIONES EMV (AIDs)
         Table('Table_v', self.metadata,
               Column('AID', String(32), nullable=False),   # identificador de aplicación (Tag 4F). Hasta 32.
               Column('Separador', String(1), nullable=False),   # "/#" (Hex. 23).
@@ -82,5 +88,15 @@ class DataBase:
               Column('Maximum Target', String(2), nullable=False),   # Para gestión de riesgos del terminal.
               Column('Etiqueta privada', String(32), nullable=False)    # Etiqueta por defecto a usar cuando el T9F12
               # o T50 no están.
+              )
+        # TABLA Z: Identifica la clave de forma univoca. Este dato se envía en el PIN-BLOCK de la trama Pufdi (‘/G’),
+        # para saber la clave que ha usado el terminal en la encriptación
+        Table('Table_Z', self.metadata,
+              Column('Id. Clave', String(1), nullable=False),   # Identifica la clave de forma univoca. Este dato se
+              # envía en el PIN-BLOCK de la trama Pufdi (‘/G’), para saber la clave que ha usado el terminal en la
+              # encriptación
+              Column('Clave', String(32), nullable=False),  # Clave de encriptación.
+              Column('KCV', String(6), nullable=False)   # Código de validación de la clave real (no del valor
+              # encriptado que se envía en la trama de telecarga) enviados en formato hexadecimal.
               )
         self.metadata.create_all(self.engine)
